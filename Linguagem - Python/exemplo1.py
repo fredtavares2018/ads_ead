e177845ec1471eea6bd78212807c9282cddc66fd
FREDTAVARES2018 <nordesttino@hotmail.com>
2020-08-31 19:46:46 -0500

    atua

--- Linguagem - Python/exemplo1.py
+++ Linguagem - Python/exemplo1.py
@@ -1,3 +1,4 @@
+
 valor = 1000
 if valor <= 1830.29:
     valor -= valor * 0.08
@@ -5,3 +6,5 @@ elif valor <= 3050.52:
     valor -= valor * 0.09
 elif valor <= 6101.06:
     valor -= valor * 0.11
+
+print(valor)
